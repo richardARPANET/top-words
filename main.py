7ae2055a14a97fd49dcb47e9e9960eb28fe32c96
richard <richard@richard.do>
2013-06-28 22:31:13 +0100

    First commit

---
+++ main.py
@@ -0,0 +1,28 @@
+from collections import Counter
+from itertools import islice
+import re
+
+
+def get_totals(words):
+    # case insensitive search
+    cnt = Counter(re.findall('\w+', words.lower()))
+    return cnt
+
+
+def take(n, iter):
+    # take first n items from an iterable
+    return list(islice(iter, n))
+
+
+def main(n):
+    # read words
+    with open('sample.txt') as f:
+        words = f.read()
+    counted_words = get_totals(words)
+    # sort by value desc
+    sorted_words = list(sorted(counted_words, key=counted_words.__getitem__, reverse=True))
+
+    return take(n, sorted_words)
+
+if __name__ == '__main__':
+    print main(15)
